b2b78e6a6d2110813ebd1cb8bc3bc1f32a8db7f8
Jairus <jairusd@codev.com>
2018-06-08 07:56:39 +0800

    wip

--- src/components/ToastMessage.jsx
+++ src/components/ToastMessage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Col, FormGroup, Alert, Button } from 'react-bootstrap'
 
-const LoaderModal = ({ onDismiss }) => (
+const LoaderModal = ({ onDismiss, message: { data, status } }) => (
   <Col smOffset={2} sm={10}>
     <FormGroup controlId='message'>
-      <Alert bsStyle='success' onDismiss={onDismiss}>
+      <Alert bsStyle={ status === 201 ? 'success' : 'danger' } onDismiss={onDismiss}>
         <h4>Success</h4>
         <p>
-          Message successfully submitted.
+          { data === 'Created' ? 'Message successfully submitted' : 'Something went wrong...' }
         </p><br />
         <Button onClick={onDismiss}>Dismiss</Button>
       </Alert>
