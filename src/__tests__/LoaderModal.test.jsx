dc4627526d2f51826981a7496f542be1afe2e02a
Jairus <jairusd@codev.com>
2018-06-18 17:14:16 +0800

    f

--- src/__tests__/LoaderModal.test.jsx
+++ src/__tests__/LoaderModal.test.jsx
@@ -4,5 +4,5 @@ import LoaderModal from '../components/LoaderModal'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  render(<LoaderModal onHide={()=>{}} show={false} />, div)
+  render(<LoaderModal onHide={() => ({})} show={false} />, div)
 })
